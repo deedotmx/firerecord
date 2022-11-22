304f9d4c809cbaadfcf85fba2c95fa8e5800cff3
Ben <ben@dee.mx>
2022-11-22 12:05:25 +0100

    first commit

---
+++ lib/firerecord.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "firerecord/version"
+
+module Firerecord
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/firerecord_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Firerecord do
+  it "has a version number" do
+    expect(Firerecord::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
